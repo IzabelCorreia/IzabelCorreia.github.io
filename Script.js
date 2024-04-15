123128bc9e3de9ebff9e517be7cf673801c72af2
IzabelCorreia <belcorreia07@gmail.com>
2024-04-15 15:24:36 -0300

    Upload Portfolio

---
+++ Script.js
@@ -0,0 +1,72 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-top', {
+    duration: 2000,
+    distance: '90px'
+})
+// SOBRE
+revelar.reveal('.efeito-sobre', {
+    duration: 2000,
+    distance: '90px',
+    origin:'left'
+})
+
+
+// PROJETOS 
+revelar.reveal('.efeito-proj1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 400
+})
+
+revelar.reveal('.efeito-proj2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 800
+})
+
+revelar.reveal('.efeito-proj3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1600
+})
+
+// CONTATO
+
+revelar.reveal('.efeito-input1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin:'top'
+})
+revelar.reveal('.efeito-input2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 600,
+    origin:'top'
+
+})
+revelar.reveal('.efeito-input3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 700,
+    origin:'top'
+
+})
+
+revelar.reveal('.textarea', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin:'top'
+
+})
+
+revelar.reveal('.efeito-bot', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin:'bottom'
+
+})
